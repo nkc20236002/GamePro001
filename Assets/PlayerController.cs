824eb9af9bf6ab15d3c6902208b61cd7a196f7d8
nkc20236002 <nkc23.kanamaru.ayumu@gmail.com>
2023-05-25 10:39:14 +0900

    Unityプロジェクトを追加

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+            
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+    }
+            
+        
+        
+            
+}
